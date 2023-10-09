b03ab726c790edaf8c07a52d953b3f866967204c
Karan Nagarkatti <karan.nagarkatti@aptean.com>
2023-10-09 16:12:34 +0530

    remove slugify

---
+++ README.md
@@ -0,0 +1,9 @@
+# emdify
+
+Prepare multiple Word files for migrating to the documentation website at once.
+
+- Converts .docx to Markdown.
+- Renames and saves images to a separate folder.
+- Refactors image links in Markdown files to the wiki-ready.
+
+Requires [Pandoc](https://pandoc.org/installing.html)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 import subprocess
-from slugify import slugify
 import shutil
 import re
+
+def slugify(s):
+    s = s.strip()
+    s = re.sub(r'[^\w\s-]', '', s)
+    s = re.sub(r'[\s_-]+', '-', s)
+    s = re.sub(r'^-+|-+$', '', s)
+    return s
 
 if __name__ == "__main__":
    path = os.path.join(os.getcwd(), "md")
