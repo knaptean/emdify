0669846fec075ffa6caccfb6f337d2f873b29659
Karan Nagarkatti <karan.nagarkatti@aptean.com>
2024-04-01 21:02:02 +0530

    add metadata variables

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import random
 import subprocess
 import shutil
 import re
+from datetime import date
+
+product = ""
+author = ""
+techwriter = ""
 
 def slugify(s):
     s = s.strip()
@@ -11,7 +16,7 @@ def slugify(s):
     s = re.sub(r'^-+|-+$', '', s)
     return s
 
-if __name__ == "__main__":
+def convert_files(file_list):
    path = os.path.join(os.getcwd(), "md")
    media_dir = os.path.join(path, "media")
 
@@ -36,7 +41,6 @@ if __name__ == "__main__":
 
    img_num_pattern = re.compile("(\d+)\.\w{3,4}(?=\")")
 
-   file_list = [f for f in os.listdir('.') if f.endswith('.docx')]
    # For each file:
    for f in file_list:
       name, ext = os.path.splitext(f)
@@ -53,15 +57,39 @@ if __name__ == "__main__":
             #img_num = re.search("(\d+)\.\w{3,4}(?=\")", str)
             return f"![image](/.attachments/{slug}{rand}{img_num})"
 
+         partial_metadata = f"product: {product}\n" \
+                    "author:\n" \
+                    f"  - {author}\n" \
+                    "tech-writer:\n" \
+                    f"  - {techwriter}\n" \
+                    "reviewer: \n" \
+                    f"  - {author}\n" \
+                    f"reviewed: {date.today()}\n" \
+                    "tags:\n" \
+                    f"  - {product}\n" \
+                    "---\n\n"
          md_text = ""
          with open(target, "r", encoding='utf-8') as md_file:
             md_text = md_file.read()
+            # if h1 exsists, use as title
+            title, md_text = md_text.split("\n", 1)
+            if title[0] == "#":
+               title = title[2:]
+            else:
+               md_text = title + "\n" + md_text
+               title = ""
 
-         with open(target, "w", encoding='utf-8') as md_file:
+            md_text = f"---\ntitle: {title}\n" + partial_metadata + md_text
             # remove escape characters for backticks
-            md_text = md_text.replace("\`", "`")
+            md_text = md_text.replace('\`', '`')
+            md_text = md_text.replace('“|”', '"')
+            # add kbd tags
+            md_text = md_text.replace('\[\[', '<kbd>')
+            md_text = md_text.replace('\]\]', '</kbd>')
             # convert img to markdown syntax and update src
             md_text = re.sub("<img src.+\/>", img_to_md, md_text)
+
+         with open(target, "w", encoding='utf-8') as md_file:
             md_file.write(md_text)
 
          # Rename images using slugified filename
@@ -73,7 +101,16 @@ if __name__ == "__main__":
    if os.path.exists(media_dir):
       os.removedirs(media_dir)
 
+if __name__ == "__main__":
+   file_list = [f for f in os.listdir('.') if f.endswith('.docx')]
    if len(file_list) > 0:
+      product = input("Product name: ")
+      author = input("Author: ")
+      techwriter = input("Tech Writer: ")
+      reviewer = input("Reviewer: ")
+      
+      convert_files(file_list)
+
       print("===Converted all files===")
    else:
       print("No files to convert!")
