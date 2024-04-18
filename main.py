90cbf95c0fafa0e2707d5590ed4a2ab9671ee482
Karan Nagarkatti <karan.nagarkatti@aptean.com>
2024-04-18 12:00:21 +0530

    Fix image issue, refactor
    Handles images when Pandoc does not adhere to extract-media flag

--- main.py
+++ main.py
@@ -17,79 +17,100 @@ def slugify(s):
     s = re.sub(r'^-+|-+$', '', s)
     return s
 
+#img_num_pattern = re.compile("(\d+)\.[A-z]{3,4}(?=\")")
+#src_filename_pattern = re.compile("[\\\/](\w+\.[A-z]{3,4})(?=\")")
+src_filename_pattern = re.compile("\/(\w+?\.[A-z]{3,4})(?=\")")
+
+# Create three directories
+# One base dir with all converted files
+base_dir = os.path.join(os.getcwd(), "md")
+if not os.path.exists(base_dir):
+   os.makedirs(base_dir)
+# One temporary dir for images that need to be renamed
+media_dir = os.path.join(base_dir, "media")
+if not os.path.exists(media_dir):
+   os.makedirs(media_dir)
+# One directory which will contain all final images
+attch_dir = os.path.join(base_dir, ".attachments")
+if not os.path.exists(attch_dir):
+   os.makedirs(attch_dir)
+
+def get_img_replacer_callback(slug, rand):
+   def inner(str):
+      img_tag = str.group()
+      img_filename = src_filename_pattern.search(img_tag).group()
+
+      # Change name of images in MD files using slug and random string
+      if (img_filename.startswith("/image")):
+         img_filename = img_filename.replace("image", f"{slug}{rand}")
+
+      return f"![image](/.attachments{img_filename})"
+
+   return inner
+
+def insert_metadata(md_text):
+   partial_metadata = f"product: {product}\n" \
+           "author:\n" \
+           f"  - {author}\n" \
+           "tech-writer:\n" \
+           f"  - {techwriter}\n" \
+           "reviewer: \n" \
+           f"  - {reviewer}\n" \
+           f"published: {date.today()}\n" \
+           f"reviewed: {date.today()}\n" \
+           "tags:\n" \
+           f"  - {product}\n" \
+           "---\n\n"
+   # if h1 exsists, use as title
+   title, md_text = md_text.split("\n", 1)
+   if title.startswith("# "):
+      title = title[2:]
+   else:
+      md_text = title + "\n" + md_text
+      title = ""
+   md_text = f"---\ntitle: {title}\n" + partial_metadata + md_text
+
+   return md_text
+
+
 def convert_files(file_list):
-   path = os.path.join(os.getcwd(), "md")
-   media_dir = os.path.join(path, "media")
-
-   rand = ''.join(random.choice("abcdefghijklmnopqrstuvwxyz23456890") for i in range(5))
-
-   ## TODO: Add metadata
-
-   ## TODO: ask to delete folder
-   ##if os.path.exists(path):
-   ##   confirm_delete = input("This folder already exists. Delete the folder (y/n):")
-   ##   if confirm_delete == "y" or confirm_delete == "Y":
-   ##      os.remove(path)
-   ##   else:
-   ##      exit("Cannot overwrite 'md' folder")
-
-   # Create a new dir, and an image dir inside it
-   if not os.path.exists(path):
-      os.makedirs(os.path.join(path, ".attachments"))
-   # Create a media dir, if it doesnt exist
-   if not os.path.exists(media_dir):
-      os.makedirs(media_dir)
-
-   img_num_pattern = re.compile("(\d+)\.\w{3,4}(?=\")")
-
    # For each file:
    for f in file_list:
       name, ext = os.path.splitext(f)
       print(f"Converting {name}...")
       if "doc" in ext:
-         # Slugify filename
+         # generate a random string to use in the filename to prevent conflicts/overwriting
+         rand = ''.join(random.choice("abcdefghijklmnopqrstuvwxyz23456890") for i in range(5))
          slug = slugify(name)
-         target = os.path.join(path, f"{slug}.md")
-         # Convert file to md and extract images
-         subprocess.run(["pandoc", "-t", "gfm", "--extract-media", path, "--wrap=none", f, "-o", target])
+         target = os.path.join(base_dir, f"{slug}.md")
 
-         def img_to_md(str):
-            img_num = img_num_pattern.search(str.group()).group()
-            #img_num = re.search("(\d+)\.\w{3,4}(?=\")", str)
-            return f"![image](/.attachments/{slug}{rand}{img_num})"
+         # Use Pandoc to convert file to md and extract images
+         subprocess.run(["pandoc", "-t", "gfm", f"--extract-media={base_dir}", "--wrap=none", f, "-o", target])
+         # NOTE:
+         # Pandoc may disregard the extract-media flag and save images to the md folder.
+         # In this case, the filenames of the images extracted are hashes instead of the
+         # usual convention of "image1.png", "image2.png", ...
+         # Move these images directly into the .attachments folder
 
-         partial_metadata = f"product: {product}\n" \
-                    "author:\n" \
-                    f"  - {author}\n" \
-                    "tech-writer:\n" \
-                    f"  - {techwriter}\n" \
-                    "reviewer: \n" \
-                    f"  - {reviewer}\n" \
-                    f"published: {date.today()}\n" \
-                    f"reviewed: {date.today()}\n" \
-                    "tags:\n" \
-                    f"  - {product}\n" \
-                    "---\n\n"
-         md_text = ""
-         with open(target, "r", encoding='utf-8') as md_file:
-            md_text = md_file.read()
-            # if h1 exsists, use as title
-            title, md_text = md_text.split("\n", 1)
-            if title[0] == "#":
-               title = title[2:]
-            else:
-               md_text = title + "\n" + md_text
-               title = ""
+         for filename in os.listdir(base_dir):
+            file = os.path.join(base_dir, filename) 
+            if (os.path.isfile(file)) and (not filename.endswith('.md')):
+               shutil.move(file, os.path.join(attch_dir, filename))
 
-            md_text = f"---\ntitle: {title}\n" + partial_metadata + md_text
+            md_text = ""
+            with open(target, "r", encoding='utf-8') as md_file:
+               md_text = insert_metadata(md_file.read())
+
             # remove escape characters for backticks
             md_text = md_text.replace('\`', '`')
             md_text = md_text.replace('“|”', '"')
             # add kbd tags
             md_text = md_text.replace('\[\[', '<kbd>')
             md_text = md_text.replace('\]\]', '</kbd>')
-            # convert img to markdown syntax and update src
-            md_text = re.sub("<img src.+\/>", img_to_md, md_text)
+
+            # Replace HTML img tags with markdown syntax and update src
+            img_replacer = get_img_replacer_callback(slug, rand)
+            md_text = re.sub("<img src.+\/>", img_replacer, md_text)
 
          with open(target, "w", encoding='utf-8') as md_file:
             md_file.write(md_text)
@@ -97,7 +118,7 @@ def convert_files(file_list):
          # Rename images using slugified filename
          for img in os.listdir(media_dir):
             renamed = f"{slug}{rand}{img.replace('image','')}"
-            shutil.move(os.path.join(media_dir, img), os.path.join(path, ".attachments", renamed))
+            shutil.move(os.path.join(media_dir, img), os.path.join(attch_dir, renamed))
 
       #print(f"...Completed {name}")
    if os.path.exists(media_dir):
